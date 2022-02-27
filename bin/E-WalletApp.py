ace547056f9ced4a1e902b884502eb1b09dc298b
BrandonW <77559638+BrandonIW@users.noreply.github.com>
2022-02-27 15:45:09 -0800

    Update E-WalletApp.py

--- bin/E-WalletApp.py
+++ bin/E-WalletApp.py
@@ -8,8 +8,9 @@ from Crypto.Cipher import AES
 from functools import wraps
 from time import sleep
 
-#TODO: Do we need a check to see if we can withdraw more than we have in the bank
-#TODO: Test sending and receiving multiple back & forth amounts to test the counter
+
+# TODO: Do we need a check to see if we can withdraw more than we have in the bank
+# TODO: Bug with a wallet that starts with 0s?
 
 def main():
     ascii_title = pyfiglet.figlet_format("E-Transfer Wallet App", font="slant")
@@ -81,15 +82,54 @@ def _wallet_additional_functions(option, wallet):
         wallet.receive_from_wallet(sender_token)
 
     elif option == 4:
-        print(f"ID: {wallet.id}\nBalance: ${wallet.balance}\nKey: {wallet.wallet_key}\n"
-              f"Synced Wallets: {[f'Wallet ID: {wal}, Counter: {count}' for wal, count in wallet.synced_wallets.items()]}")
-        sleep(1)
+        sync_option = _print_sync_menu(wallet)
+        if sync_option == 1:
+            receiver_id = input("Input Wallet ID of Recipient: ")
+            while True:
+                if not _validate_wallet_id(receiver_id):
+                    receiver_id = input("Invalid Wallet ID. Must be 4 digits i.e. 7171. Try again: ")
+                    continue
+                break
+            wallet.create_sync_token(receiver_id)
+        else:
+            sender_token = input("Please enter Sender's Sync token: ")
+            wallet.receive_sync_token(sender_token)
 
     elif option == 5:
+        print(f"ID: {wallet.id}\nBalance: ${wallet.balance}\nKey: {wallet.wallet_key}\n"
+              f"Synced Wallets: {[f'Wallet ID: {wal}, Counter: {count}' for wal, count in wallet.synced_wallets.items()]}\n")
+        sleep(1)
+
+    elif option == 6:
         return
 
 
 #####________Menu Printing Functions________######
+def _print_sync_menu(wallet):
+    print(f"Working with: {wallet}\n")
+
+    menu_options = {
+        1: 'Create Sync Token',
+        2: 'Receive Sync Token'
+    }
+
+    for key in menu_options.keys():
+        print(key, '-', menu_options[key])
+    print("\n")
+
+    while True:
+        try:
+            option = int(input("Enter your choice: "))
+            if option not in menu_options.keys():
+                print("Invalid option. Choose the wallet number i.e. 0, 1, 2 etc.\n")
+                sleep(1)
+                continue
+            return option
+        except ValueError:
+            print("Invalid option. Choose the wallet number i.e. 0, 1, 2 etc.\n")
+            sleep(1)
+            continue
+
 
 def _print_wallet_functions_menu(wallet):
     print(f"Working with: {wallet}\n")
@@ -98,8 +138,9 @@ def _print_wallet_functions_menu(wallet):
         1: 'Deposit Bank EMD',
         2: 'Send Funds to another Wallet',
         3: 'Receive Funds from another Wallet',
-        4: 'Show Information About Wallet',
-        5: 'Back',
+        4: 'Synchronize Wallets',
+        5: 'Show Information About Wallet',
+        6: 'Back',
     }
 
     for key in menu_options.keys():
@@ -107,12 +148,17 @@ def _print_wallet_functions_menu(wallet):
     print("\n")
 
     while True:
-        option = int(input("Enter your choice: "))
-        if option not in menu_options.keys():
+        try:
+            option = int(input("Enter your choice: "))
+            if option not in menu_options.keys():
+                print("Invalid option. Choose the wallet number i.e. 0, 1, 2 etc.\n")
+                sleep(1)
+                continue
+            return option
+        except ValueError:
             print("Invalid option. Choose the wallet number i.e. 0, 1, 2 etc.\n")
             sleep(1)
             continue
-        return option
 
 
 def _print_wallet_menu(wallet_list):
@@ -124,18 +170,23 @@ def _print_wallet_menu(wallet_list):
     print("\n")
 
     while True:
-        wallet_choice = int(input("Enter your choice: "))
-        if wallet_choice not in menu_options.keys():
+        try:
+            wallet_choice = int(input("Enter your choice: "))
+            if wallet_choice not in menu_options.keys():
+                print("Invalid option. Choose the wallet number i.e. 0, 1, 2 etc.\n")
+                sleep(1)
+                continue
+            return menu_options[wallet_choice]
+        except ValueError:
             print("Invalid option. Choose the wallet number i.e. 0, 1, 2 etc.\n")
             sleep(1)
             continue
-        return menu_options[wallet_choice]
 
 
 def _print_main_menu():
     menu_options = {
         1: 'Create a New Wallet',
-        2: 'Send/Receive Funds or Show Information about a Wallet',
+        2: 'Wallet Functions (Send/Receive Funds, Show Info, Synchronize)',
         3: 'List all Wallets',
         4: 'Exit Application',
     }
@@ -205,25 +256,57 @@ class Wallet:
     def __repr__(self):
         return f"Wallet-{self.id}"
 
-    def sync_wallets(self, receiver_id):
-        if receiver_id not in self.synced_wallets.keys():
-            print("Wallet not yet synced. Syncing Wallet...")
+    def create_sync_token(self, receiver_id):
+        sync_token = self._token_generator(self.id, receiver_id, 0, 0, self.bank_key)
+        print(f"Your Sync Token for Wallet {receiver_id} is: {sync_token}. Run this Token on the Recipient Wallet "
+              f"before proceeding with sending cash, else the recipient will not receive the cash\n")
+
+    def receive_sync_token(self, token):
+        token_bytes = bytes.fromhex(token)
+        bank_key_bytes = bytes.fromhex(self.bank_key)
+
+        cipher = AES.new(bank_key_bytes, AES.MODE_ECB)
+        plaintext = cipher.decrypt(token_bytes).hex()
+        sender_id, receiver_id, amount, counter = [int(plaintext[idex:idex + 8], 16) for idex in range(0, 32, 8)]
+        if str(receiver_id) != self.id:
+            print(f"Sync Token contains recipient Wallet ID of {receiver_id}. This does not match your wallet ID of "
+                  f"{self.id}. Ignoring Token\n")
+            sleep(1)
+            return
+
+        if str(sender_id) not in self.synced_wallets.keys():
+            print("Synchronizing Wallet...")
+            sleep(1)
+            self.synced_wallets[str(sender_id)] = 1
+            print(f"Added Wallet ID-{str(sender_id)} as a Sync'd Wallet\n")
             sleep(1)
 
-            self.synced_wallets[receiver_id] = 0
-            sync_token = self._token_generator(self.id, receiver_id, 0, 0, self.bank_key)
+            while True:
+                choice = input(f"Do you want to make a Sync Token for Wallet ID-{sender_id}? (y/n)")
+                if choice.lower() in ['y', 'yes']:
+                    self.create_sync_token(str(sender_id))
+                    return
+                elif choice.lower() in ['n', 'no']:
+                    return
+                else:
+                    print("Invalid response. Type 'y', 'yes', 'n', or 'no'\n")
+                    sleep(1)
 
-            print(f"Your Sync Token for Wallet {receiver_id} is: {sync_token}. Run this Token on the Recipient Wallet "
-                  f"before proceeding with sending cash, else the recipient will not receive the cash\n")
+        print(
+            f"Wallet ID-{str(sender_id)} is already synced. Current counter is {self.synced_wallets[str(sender_id)]}\n")
+
+    def check_sync_wallets(self, receiver_id):
+        if receiver_id not in self.synced_wallets.keys():
+            print(f"Wallet ID-{receiver_id} is not yet synced. Please go back to the Wallet Menu and select the Sync "
+                  f"option")
+            sleep(3)
             return False
-
-        self.synced_wallets[receiver_id] += 1
         return self.synced_wallets[receiver_id]
 
     def recieve_from_bank(self, emd_token_hex):
         dollars_hex = self._decrypt_emd(emd_token_hex, self.wallet_key)
         self.balance += int(dollars_hex, 16)
-        print(f"Deposited ${int(dollars_hex, 16)}. New wallet balance is ${self.balance}")
+        print(f"Deposited ${int(dollars_hex, 16)}. New wallet balance is ${self.balance}\n")
 
     def send_to_wallet(self):
         receiver_id = input("Input Wallet ID of Recipient: ")
@@ -233,7 +316,7 @@ class Wallet:
                 continue
             break
 
-        counter = self.sync_wallets(receiver_id)
+        counter = self.check_sync_wallets(receiver_id)
         if not counter:
             return
 
@@ -247,7 +330,8 @@ class Wallet:
 
         self.balance -= int(validated_dollar_amount)
         token = self._token_generator(self.id, receiver_id, validated_dollar_amount, counter, self.bank_key)
-        print(f"Your token is: {token} | New account balance is ${self.balance}")
+        print(f"Your token is: {token} | New account balance is ${self.balance}\n")
+        self.synced_wallets[receiver_id] += 1
 
     def receive_from_wallet(self, token):
         token_bytes = bytes.fromhex(token)
@@ -257,31 +341,30 @@ class Wallet:
         plaintext = cipher.decrypt(token_bytes).hex()
         sender_id, receiver_id, amount, counter = [int(plaintext[idex:idex + 8], 16) for idex in range(0, 32, 8)]
 
-        if sender_id not in self.synced_wallets:
-            self.synced_wallets[sender_id] = 1
-            print(f"Wallet {sender_id} has been added to list of synced wallets")
+        if str(sender_id) not in self.synced_wallets:
+            print(f"Wallet ID-{sender_id} has not yet been Synchronized. Please Sync this wallet first from the Wallet"
+                  f"Menu\n")
             sleep(2)
             return
 
         if not self._validate_counter(sender_id, counter):
-            print(f"Token contains counter of {counter}. Does not match wallet {sender_id}'s counter of "
-                  f"{self.synced_wallets[sender_id]}. Possible replay attack.")
+            print(f"Token contains counter of {counter}. Does not match Wallet ID-{sender_id}'s counter of "
+                  f"{self.synced_wallets[str(sender_id)]}. Possible replay attack.")
             sleep(1)
-            print(f"Dropping data and will not deposit. Current Wallet Balance: ${self.balance}")
+            print(f"Dropping token and will not deposit. Current Wallet Balance: ${self.balance}\n")
             return
 
         if str(receiver_id) != self.id:
-            print(f"Token's recipient ID of {receiver_id} does not match Wallet ID of {self.id}. Dropping Token")
+            print(f"Token's recipient ID of {receiver_id} does not match Wallet ID of {self.id}. Dropping Token\n")
             return
 
-        self.synced_wallets[sender_id] += 1
         self.balance += amount
-        print(f"Deposited ${amount}")
+        self.synced_wallets[str(sender_id)] += 1
+        print(f"Deposited ${amount}. Your new Wallet balance is: ${self.balance}\n")
         sleep(1)
-        print(f"Your new Wallet balance is: {self.balance}")
 
     def _validate_counter(self, sender_id, counter):
-        return True if self.synced_wallets[sender_id] == counter else False
+        return True if self.synced_wallets[str(sender_id)] == counter else False
 
     @staticmethod
     @hex_creator
